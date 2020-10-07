3dfd1d689b20f8b1cf480c35efc6194fa219cd06
sunniraleigh <sraleigh@smith.edu>
2020-10-07 17:06:03 +0000

    implement recommendations

--- sales_report.py
+++ sales_report.py
@@ -1,31 +1,27 @@
-"""Generate sales report showing total melons each salesperson sold."""
+def report_sales(file_path):
+    """ Generate sales report showing total melons each salesperson sold."""
 
+    sales = {}
 
-salespeople = [] #generates an empty list
-melons_sold = [] #generates an empty list
+    file = open(file_path) # opens text files into a reference called f
 
-f = open('sales-report.txt') # opens text files into a reference called f
-for line in f: # goes through each line in the file
-    line = line.rstrip() #strips white space from each line
-    entries = line.split('|') #turns each line into a list
+    for line in file: # goes through each line in the file
+        line = line.rstrip() #strips white space from each line
+        entries = line.split('|') #turns each line into a list
 
-    salesperson = entries[0] #identifies salesperson in the list entries
-    melons = int(entries[2]) # identifies melons sold by a saleperson in the list entries
+        salesperson = entries[0] #identifies salesperson in the list entries
+        melons = int(entries[2]) # identifies melons sold by a saleperson in the list entries
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson) #if the saleperson is already in a list, get the index of that salesperson
+        sales[salesperson] = sales.get(salesperson, 0) + melons
 
-        melons_sold[position] += melons #add to the number of melons that salesperson has sold
-    else:
-        salespeople.append(salesperson) #if the salesperson was not in the list, add them
-        melons_sold.append(melons) # add the number of melons they sold
+    for salesperson, melons in sales.items():
+        print(f"{salesperson} sold {melons} melons")
 
-
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons') #for each salesperson, print their name and how many melons they sold
+report_sales("sales-report.txt")
 
 """ List of improvements:
     - utilize dictionary methods to do lines 15-21 in one line
-    - name variables more descriptively such as 'f'
-
+    - name variables more descriptively such as 'f' and 'i'
+    - define whole process as a function
+    - add data to a dictionary rather than 2 separate lists
 """
